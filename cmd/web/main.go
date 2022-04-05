bf7bc2249120035f110e264c17baab9106793d81
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-04-05 18:38:33 +0200

    Initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/alexedwards/scs/v2"
+	"github.com/lukaszczajkowski/golang-bookings/pkg/config"
+	"github.com/lukaszczajkowski/golang-bookings/pkg/handlers"
+	"github.com/lukaszczajkowski/golang-bookings/pkg/render"
+	"log"
+	"net/http"
+	"time"
+)
+
+// const cannot be overriden, and var can
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+// main is the main application function
+func main() {
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	// in production set to true
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	// that is needed when we're not using external routing packages
+	//http.HandleFunc("/", handlers.Repo.Home)
+	//http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	// start server on port :8080
+	// _ = means if there is an error, I don't care
+	//_ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/lukaszczajkowski/golang-bookings/pkg/config"
+	"github.com/lukaszczajkowski/golang-bookings/pkg/handlers"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	//mux := pat.New()
+
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	// middleware to avoid panic and allow graceful error recovery
+	mux.Use(middleware.Recoverer)
+	// just an example of using a custom middleware
+	//mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
